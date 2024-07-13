c96b8726aba6d358f226a28c22d7a19130c008e0
Le-HoangAnh <hoanganh1012013@gmail.com>
2024-07-13 16:08:06 +0700

    1. up part 1 of game

---
+++ Assets/Project/Scripts/LevelButton.cs
@@ -0,0 +1,57 @@
+using Connect.Core;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Connect.Core
+{
+    public class LevelButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private Image _image;
+        [SerializeField] TMP_Text _levelText;
+        [SerializeField] private Color _inactiveColor;
+
+        private bool isLevelUnlocked;
+        private int currentLevel;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(Clicked);
+        }
+
+        private void OnEnable()
+        {
+            MainMenuManager.Instance.LevelOpened += LevelOpened;
+        }
+
+        private void OnDisable()
+        {
+            MainMenuManager.Instance.LevelOpened -= LevelOpened;
+        }
+
+        private void LevelOpened()
+        {
+            string gameObjectName = gameObject.name;
+            string[] parts = gameObjectName.Split('_');
+            _levelText.text = parts[parts.Length - 1];
+            currentLevel = int.Parse(_levelText.text);
+            isLevelUnlocked = GameManager.Instance.IsLevelUnlocked(currentLevel);
+
+            _image.color = isLevelUnlocked ? MainMenuManager.Instance.CurrentColor : _inactiveColor;
+        }
+
+        private void Clicked()
+        {
+            if (!isLevelUnlocked)
+            {
+                return;
+            }
+
+            GameManager.Instance.CurrentLevel = currentLevel;
+            GameManager.Instance.GoToGamePlay();
+        }
+    }
+}
